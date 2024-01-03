7210d3aca56f439119dd8ed967e7f0c1d1c45bf3
SUMIT <skr.learn@outlook.com>
2024-01-03 13:14:04 +0530

    commit

--- dbt_snowflake_airbnb/models/fact/fact_reviews.sql
+++ dbt_snowflake_airbnb/models/fact/fact_reviews.sql
@@ -8,8 +8,13 @@
 WITH fact_reviews AS (
     SELECT * FROM {{ ref("src_reviews") }}
 )
-SELECT * FROM fact_reviews
-WHERE review_text IS NOT NULL
+SELECT 
+    {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'review_text']) }} AS review_id, 
+    * 
+FROM 
+    fact_reviews
+WHERE 
+    review_text IS NOT NULL
 
 {% if is_incremental() %}
  AND review_date > (SELECT MAX(review_date) from {{ this }})
